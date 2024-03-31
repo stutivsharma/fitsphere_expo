7d49d448054cdbdd5add4da6709df4bc76ea2391
stutivsharma <135279335+stutivsharma@users.noreply.github.com>
2024-03-30 18:38:09 -0700

    first exercise

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ export default function App() {
   console.log(exercise);
   return (
     <View style={styles.container}>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <Text style={styles.exerciseName}>{exercise.muscle}</Text>
-      <Text style={styles.exerciseName}>{exercise.equipment}</Text>
-      <StatusBar style="auto" />
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>{exercise.muscle}</Text>
+        <Text style={styles.exerciseSubtitle}>{exercise.equipment}</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -18,14 +20,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "gainsboro",
     alignItems: "center",
     justifyContent: "center",
   },
 
+  exerciseContainer: {
+    backgroundColor: "white",
+    padding: 12,
+    borderRadius: 10,
+    gap: 5,
+  },
+
   exerciseName: {
     fontSize: 24,
-    fontWeight: 700,
+    fontWeight: 500,
+    color: "black",
+  },
+
+  exerciseSubtitle: {
+    fontsize: 16,
     color: "dimgray",
   },
 });
